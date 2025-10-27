40a800586586ed3dcdad2e784528dd80fe424db3
Rohit Rode <roderohit071@gmail.com>
2025-10-27 23:00:14 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://appplanner-1.onrender.com',
+    baseURL: 'https://appplanner-1.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
